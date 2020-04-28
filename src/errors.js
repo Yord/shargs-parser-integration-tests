01c5c1b595059453fd28210f4c92acf89eb3fcf7
Philipp Wille <philipp.y.wille@gmail.com>
2020-04-28 14:23:49 +0200

    feat: update integration tests to new versions

--- src/errors.js
+++ src/errors.js
@@ -1,77 +1,77 @@
 const argumentIsNotABool = ({values, defaultValues, index, option}) => ({
-  code: 'Argument is not a boolean',
+  code: 'ArgumentIsNotABoolean',
   msg:  "The passed command line argument must either be 'true' or 'false'",
   info: {values, defaultValues, index, option}
 })
 
 const commandRequired = ({options}) => ({
-  code: 'Command required',
+  code: 'CommandRequired',
   msg:  'No command found. Please use at least one command!',
   info: {options}
 })
 
 const contradictionDetected = ({key, contradicts, option}) => ({
-  code: 'Contradiction detected',
+  code: 'ContradictionDetected',
   msg:  'Some given keys contradict each other.',
   info: {key, contradicts, option}
 })
 
 const didYouMean = ({argv, options}) => ({
-  code: 'Did you mean',
+  code: 'DidYouMean',
   msg:  'An unknown command-line argument was passed. Did you mean any of the following options?',
   info: {argv, options}
 })
 
 const falseArgsRules = ({rules, args}) => ({
-  code: 'False args rules',
+  code: 'FalseArgsRules',
   msg:  'Your args rules returned false. Please abide to the rules defined in verifyArgs.',
   info: {rules, args}
 })
 
 const falseArgvRules = ({rules, argv}) => ({
-  code: 'False argv rules',
+  code: 'FalseArgvRules',
   msg:  'Your argv rules returned false. Please abide to the rules defined in verifyArgv.',
   info: {rules, argv}
 })
 
 const falseOptsRules = ({rules, options}) => ({
-  code: 'False opts rules',
+  code: 'FalseOptsRules',
   msg:  'Your opts rules returned false. Please abide to the rules defined in verifyOpts.',
   info: {rules, options}
 })
 
 const falseRules = ({key, rules, option}) => ({
-  code: 'False rules',
+  code: 'FalseRules',
   msg:  "An option's rules returned false. Please check your arguments.",
   info: {key, rules, option}
 })
 
 const implicationViolated = ({key, implies, option}) => ({
-  code: 'Implication violated',
+  code: 'ImplicationViolated',
   msg:  'Some given keys that imply each other are not all defined.',
   info: {key, implies, option}
 })
 
 const invalidRequiredPositionalArgument = ({positionalArguments}) => ({
-  code: 'Invalid required positional argument',
+  code: 'InvalidRequiredPositionalArgument',
   msg:  'If a positional argument is required, all previous positional arguments must be required as well. The required field must either be undefined, true or false.',
   info: {positionalArguments}
 })
 
 const requiredOptionMissing = ({key, option}) => ({
-  code: 'Required option is missing',
+  code: 'RequiredOptionIsMissing',
   msg:  'An option that is marked as required has not been provided.',
   info: {key, option}
 })
 
 const unexpectedArgument = ({argument}) => ({
-  code: 'Unexpected argument',
+  code: 'UnexpectedArgument',
   msg:  'An unexpected argument was used that has no option defined.',
   info: {argument}
 })
 
 const valueRestrictionsViolated = ({key, values, index, only, option}) => ({
-  code: 'Value restriction violated',
+  code: 'ValueRestrictionViolated',
   msg:  'A value lies outside the allowed values of an option.',
   info: {key, values, index, only, option}
 })
